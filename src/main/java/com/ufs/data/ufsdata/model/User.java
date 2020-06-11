05c7a20ee1f79d8fb8e55e5e672b31484423be04
Winston <xu914983053@icloud.com>
2020-06-11 17:00:02 +0800

    integrated with mysql and mybatis

--- src/main/java/com/ufs/data/ufsdata/model/User.java
+++ src/main/java/com/ufs/data/ufsdata/model/User.java
@@ -4,8 +4,65 @@ import lombok.Data;
 
 @Data
 public class User {
+    private Integer id;
 
-    private Long id;
+    private String openid;
 
-    private String name;
+    private String nickname;
+
+    private String avatar;
+
+    private String restraunt;
+
+    private String qqoid;
+
+    private String mobile;
+
+    private String wxuid;
+
+    private Integer isofficial;
+
+    private Integer created_at;
+
+    private Integer last_modified;
+
+    private String jointname;
+
+    private String aid;
+
+    private String recommendid;
+
+    private Short point_issued;
+
+    private Integer unread_count;
+
+    private String lasttoken;
+
+    private Integer lasttoken_at;
+
+    private Integer na;
+
+    private Integer banned;
+
+    private Integer recover_time;
+
+    private String city;
+
+    private String job_postion;
+
+    private Integer is_followed;
+
+    private Integer used;
+
+    private String wx_openid;
+
+    private Integer perecfet_from;
+
+    private Integer push_message;
+
+    private String login_from;
+
+    private Integer dealer_id;
+
+    private Integer created_from;
 }

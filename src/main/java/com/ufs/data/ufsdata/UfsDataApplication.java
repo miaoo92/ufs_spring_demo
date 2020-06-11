68d19ec2dc8650b7f3908694ff80d86c6fac446e
Winston <xu914983053@icloud.com>
2020-06-11 09:46:14 +0800

    init projext

---
+++ src/main/java/com/ufs/data/ufsdata/UfsDataApplication.java
@@ -0,0 +1,13 @@
+package com.ufs.data.ufsdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfsDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UfsDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ufs/data/ufsdata/UfsDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufs.data.ufsdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UfsDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
